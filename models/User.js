f741139d6a728db87548ed7771305e9702cec37c
sojogomoz <sojogomoz@gmail.com>
2025-05-03 12:10:15 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,12 +1,18 @@
-
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-  phone: { type: String, required: true, unique: true },
-  passwordHash: { type: String, required: true },
-  balance: { type: Number, default: 0 },
-  createdAt: { type: Date, default: Date.now },
-});
+const userSchema = new mongoose.Schema(
+  {
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.models.User || mongoose.model('User', UserSchema);
-export default User;
+export default mongoose.models.User || mongoose.model('User', userSchema);
