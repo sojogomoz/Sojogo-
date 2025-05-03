e5ee27d96ebca78c0bf87ea79890099f8f4fa39e
sojogomoz <sojogomoz@gmail.com>
2025-05-03 09:51:20 +0200

    lib/

---
+++ dbConnect.js
@@ -0,0 +1,12 @@
+
+import mongoose from 'mongoose';
+
+const dbConnect = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+  await mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
+
+export default dbConnect;
